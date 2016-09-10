d1304e13dc8f9d9667816cb70d99ef49211a8d8b
Donghun <dkang9@gmail.com>
2016-09-10 16:10:43 -0400

    pushing templates

--- LyftPriceFinder/www/js/app.js
+++ LyftPriceFinder/www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('lyftpricefinder', ['ionic','lyftpricefinder.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -22,3 +22,33 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+  .state('tab', {
+    url: '/tab',
+    abstract: true,
+    templateUrl: 'templates/tabs.html',
+    controller: 'TabsCtrl'
+  })
+
+  .state('tab.map', {
+    url: '/map',
+    views: {
+      'tab-map': {
+        templateUrl: 'templates/map.html',
+        controller: 'MapCtrl'
+      }
+    }
+  })
+
+  $urlRouterProvider.otherwise('/tab/map');
+
+})
+
+.constant('SERVER', {
+  // Local server
+  //url: 'http://localhost:3000'
+  url: 'http://localhost:3000'
+});
---
+++ LyftPriceFinder/www/js/controllers.js
@@ -0,0 +1,19 @@
+angular.module('lyftpricefinder.controllers', ['ionic'])
+
+/*
+Controller for the map page
+*/
+.controller('MapCtrl', function($scope) {
+	//$scope.giveTrip(start,end) = function(float,float) {
+		//start 
+	//}
+
+})
+
+
+/*
+Controller for our tab bar
+*/
+.controller('TabsCtrl', function($scope) {
+
+});
